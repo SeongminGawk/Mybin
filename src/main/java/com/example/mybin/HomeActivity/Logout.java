0a7309fa0d12a724ba2febaa4db9c499bbf01db9
tjdals <84428695+SeongminGawk@users.noreply.github.com>
2024-12-01 16:29:44 +0900

    로그아웃 기능 및 세션 초기화 처리

--- src/main/java/com/example/mybin/HomeActivity/Logout.java
+++ src/main/java/com/example/mybin/HomeActivity/Logout.java
@@ -5,9 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.mybin.Membership.LoginScreen;
 
 public class Logout extends AppCompatActivity {
@@ -20,22 +18,20 @@ public class Logout extends AppCompatActivity {
 
     private void logout() {
         showToast("로그아웃되었습니다.");
-        clearUserData(); // 모든 사용자 데이터 삭제
+        clearUserData(); 
         Intent intent = new Intent(Logout.this, LoginScreen.class);
         startActivity(intent);
         finish();
     }
 
-    // 메시지 표시
     private void showToast(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
-
-    // SharedPreferences에서 모든 사용자 데이터 삭제
+    
     private void clearUserData() {
         SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.clear(); // 모든 데이터 삭제
+        editor.clear();
         editor.apply();
     }
 }
